0df06d6f39625fcd2de1c3d17c185fccabee42de
Agus Puryanto <aguspuryanto@gmail.com>
2025-12-25 10:50:38 +0700

    first commit

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'app_key' => '6frt0hopcl3dl',
-    'app_secret' => '78fcd055db1f22403304b68500417f4f3561996e',
-    'shop_cipher' => 'ROW_PyC2jgAAAAARCXp_TXWmOhxFE7PrDELy',
+    'app_key' => 'xxx',
+    'app_secret' => 'xxx',
+    'shop_cipher' => 'xxx',
     'api_version' => '202309',
     'redirect_uri' => 'http://localhost/tiktokshop/callback.php'
 ];
